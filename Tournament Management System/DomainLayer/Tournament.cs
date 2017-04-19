b3d4eece12316fba07619faee30c0c0800fb7c8a
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2017-04-19 09:54:07 +0200

    Player and Tournament class added

---
+++ Tournament Management System/DomainLayer/Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    class Tournament
+    {
+        public string LeagueName { get; set; }
+        public List<Player> PlayerList { get; set; }
+        public string Reward{ get; set; }
+        public int Rounds{ get; set; }
+        public string GameName{ get; set; }
+    }
+}
